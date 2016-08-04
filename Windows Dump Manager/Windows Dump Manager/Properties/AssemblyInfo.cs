6891b54b1972a56c030996f7bf60329f2e5765a4
Kunal Chowdhury <kunal@diduce.com>
2016-08-04 17:04:52 +0530

    Initial version of "Windows Dump Manager" - version: 2016.08.04.02

---
+++ Windows Dump Manager/Windows Dump Manager/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Windows Dump Manager")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("www.kunal-chowdhury.com")]
+[assembly: AssemblyProduct("Windows Dump Manager")]
+[assembly: AssemblyCopyright("Copyright © 2016 Kunal Chowdhury")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("90541b4f-656c-4218-9e23-f7dd3a13e6e8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2016.08.04.02")]
+[assembly: AssemblyFileVersion("2016.08.04.02")]
