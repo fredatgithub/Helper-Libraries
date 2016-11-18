f705843138e697eadcabbe9628258ce6ef15ce85
Kunal Chowdhury <Kunal Chowdhury>
2016-11-18 16:31:14 +0530

    Added 'DumpHelper' library

---
+++ src/Helpers/Dump/Enums/DumpType.cs
@@ -0,0 +1,21 @@
+﻿namespace KCSoft.Windows.Helpers.Dump.Enums
+{
+    public enum DumpType
+    {
+        MiniDumpNormal = 0,
+        MiniDumpWithDataSegs = 1,
+        MiniDumpWithFullMemory = 2,
+        MiniDumpWithHandleData = 4,
+        MiniDumpFilterMemory = 8,
+        MiniDumpScanMemory = 16,
+        MiniDumpWithUnloadedModules = 32,
+        MiniDumpWithIndirectlyReferencedMemory = 64,
+        MiniDumpFilterModulePaths = 128,
+        MiniDumpWithProcessThreadData = 256,
+        MiniDumpWithPrivateReadWriteMemory = 512,
+        MiniDumpWithoutOptionalData = 1024,
+        MiniDumpWithFullMemoryInfo = 2048,
+        MiniDumpWithThreadInfo = 4096,
+        MiniDumpWithCodeSegs = 8192,
+    }
+}
