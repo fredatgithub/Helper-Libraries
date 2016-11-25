4cc6bdd1a11f74c9427997599f20daa76089c9a7
Kunal Chowdhury <kunal.chowdhury@outlook.com>
2016-11-25 14:40:23 +0530

    API to check whether a specified assembly file is signed
    - Added 'WinLab.Windows.Helpers.IO.AssemblyHelper.IsSigned(filepath)' to track the sign status

--- projects/WinLab.Windows.Helpers.IO/AssemblyHelper.cs
+++ projects/WinLab.Windows.Helpers.IO/AssemblyHelper.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
@@ -26,6 +28,24 @@ namespace WinLab.Windows.Helpers.IO
         public static Version GetAssemblyVersion(Assembly assembly)
         {
             return GetAssemblyName(assembly).Version;
+        }
+
+        public static bool IsSigned(string filepath)
+        {
+            var runspaceConfiguration = RunspaceConfiguration.Create();
+            using (var runspace = RunspaceFactory.CreateRunspace(runspaceConfiguration))
+            {
+                runspace.Open();
+                using (var pipeline = runspace.CreatePipeline())
+                {
+                    pipeline.Commands.AddScript("Get-AuthenticodeSignature \"" + filepath + "\"");
+                    var results = pipeline.Invoke();
+                    runspace.Close();
+
+                    var signature = results[0].BaseObject as Signature;
+                    return signature == null || signature.SignerCertificate == null ? false : (signature.Status != SignatureStatus.NotSigned);
+                }
+            }
         }
     }
 }
