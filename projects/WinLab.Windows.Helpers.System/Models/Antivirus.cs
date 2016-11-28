430a1f0bf17b027e627d83eb6b20d5b0201b6f3b
Kunal Chowdhury <kunal.chowdhury@outlook.com>
2016-11-28 17:37:02 +0530

    Added 'Anti Virus' software detection API sets as part of 'WinLab.Windows.Helpers.System.SystemHelper.cs' class file

---
+++ projects/WinLab.Windows.Helpers.System/Models/Antivirus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace WinLab.Windows.Helpers.System.Models
+{
+    [Guid("63D4EE8E-90CA-4C8D-9C8D-1701090E5637")]
+    public class Antivirus
+    {
+        public string ID { get; set; }
+
+        public string DisplayName { get; set; }
+    }
+}
---
+++ projects/WinLab.Windows.Helpers.System/SystemHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Management;
+using System.Runtime.InteropServices;
+using WinLab.Windows.Helpers.System.Models;
+
+namespace WinLab.Windows.Helpers.System
+{
+    [Guid("12DD2C8D-F4B4-46B6-8899-230B418A00F0")]
+    public class SystemHelper
+    {
+        private const string SecurityCenterLocation = @"\root\SecurityCenter";
+        private const string SecurityCenter2Location = @"\root\SecurityCenter2";
+        private const string AntivirusProductSearchQuery = "SELECT * FROM AntivirusProduct";
+
+        public static bool IsAntivirusInstalled()
+        {
+            return GetInstalledAntivirusProducts().Count > 0;
+        }
+
+        public static ObservableCollection<Antivirus> GetAntivirusDetails()
+        {
+            return GetInstalledAntivirusProducts();
+        }
+
+        private static ObservableCollection<Antivirus> GetInstalledAntivirusProducts()
+        {
+            var antivirusCollection = new ObservableCollection<Antivirus>();
+            using (var searcher = new ManagementObjectSearcher(@"\\" + Environment.MachineName + SecurityCenterLocation, AntivirusProductSearchQuery))
+            {
+                var searcherInstance = searcher.Get();
+                foreach (var instance in searcherInstance)
+                {
+                    antivirusCollection.Add(new Antivirus { ID = instance["instanceGuid"].ToString(), DisplayName = instance["displayName"].ToString() });
+                }
+            }
+
+            using (var searcher = new ManagementObjectSearcher(@"\\" + Environment.MachineName + SecurityCenter2Location, AntivirusProductSearchQuery))
+            {
+                var searcherInstance = searcher.Get();
+                foreach (var instance in searcherInstance)
+                {
+                    antivirusCollection.Add(new Antivirus { ID = instance["instanceGuid"].ToString(), DisplayName = instance["displayName"].ToString() });
+                }
+            }
+
+            return antivirusCollection;
+        }
+    }
+}
