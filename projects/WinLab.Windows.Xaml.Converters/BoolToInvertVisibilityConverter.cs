9cff61fa8f7bd5de83800eaf14f7053253f9986f
Kunal Chowdhury <kunal.chowdhury@outlook.com>
2016-12-16 14:22:22 +0530

    Added 'BoolToInvertVisibilityConverter'

---
+++ projects/WinLab.Windows.Xaml.Converters/BoolToInvertVisibilityConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WinLab.Windows.Xaml.Converters
+{
+    public class BoolToInvertVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var boolValue = System.Convert.ToBoolean(value);
+            return boolValue ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var visiblityValue = (Visibility)value;
+            return visiblityValue != Visibility.Visible;
+        }
+    }
+}
