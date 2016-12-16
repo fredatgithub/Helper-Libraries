cf618ebdc6873364e8d1c2cbe46228bc275a566a
Kunal Chowdhury <kunal.chowdhury@outlook.com>
2016-12-16 14:20:20 +0530

    Added 'BoolToVisibilityConverter' and 'BoolToVisibilityConverterEx'

---
+++ projects/WinLab.Windows.Xaml.Converters/BoolToVisibilityConverterEx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WinLab.Windows.Xaml.Converters
+{
+    public class BoolToVisibilityConverterEx : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Equals(value, parameter ?? When) ? Value: Otherwise;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object When { get; set; }
+
+        public object Value { get; set; }
+
+        public object Otherwise { get; set; }
+    }
+}
